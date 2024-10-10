e77932c6edd87c8dff27b8329aad24b581249e06
Val14zp <valery14zp@gmail.com>
2024-10-10 00:51:26 -0600

    conexión completa con consulta de lecturas a disco

--- data_base/database.R
+++ data_base/database.R
@@ -1,28 +1,26 @@
 library(RJDBC)
 library(DBI)
-library(config) # Ensure you have the config library loaded
+
 
 # Load JDBC driver
 driver <- JDBC(driverClass = "oracle.jdbc.OracleDriver", "C:/drivers/oracle/ojdbc11.jar")
 
 # SQL query to fetch data from the ASEGURADO table
-consulta_asegurado <- "SELECT * FROM ASEGURADO;"
+consulta_lectura_disco <- "SELECT 
+    name, 
+    value 
+FROM 
+    v$sysstat 
+WHERE 
+    name IN ('physical reads', 'physical writes')
+"
 
-# Define a function to establish a database connection
 database_get_connect <- function() {
-  # Load configuration from config.yml
-  xedb <- config::get(file = "config.yml") # Ensure correct path to config file
-  
-  # Ensure that xedb is a list and has the required fields
-  if (is.list(xedb) && all(c("host", "port", "sid", "user", "pass") %in% names(xedb))) {
     dbCon <- dbConnect(driver,
-                       paste0("jdbc:oracle:thin:@", xedb$host, ":", xedb$port, ":", xedb$sid), # Connection string
-                       xedb$user, # Username
-                       xedb$pass) # Password
+                       "jdbc:oracle:thin:@localhost:1521:xe",
+                       "sys as sysdba", # Username con rol
+                       "root") # Password
     return(dbCon) # Return the database connection
-  } else {
-    stop("Invalid configuration format. Ensure the config file is properly structured.")
-  }
 }
 
 # Establish connection to the database
